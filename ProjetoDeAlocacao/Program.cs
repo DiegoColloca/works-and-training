a334afc6f9cd68477a7d6cebdd614d8d5ed95932
Diego Colloca <diego.colloca@esx.com.br>
2019-02-11 12:02:28 -0200

    Adicionar arquivos de projeto.

---
+++ ProjetoDeAlocacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoDeAlocacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
